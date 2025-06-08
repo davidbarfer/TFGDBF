0936acd809edd781ff475ed1db5a11a4de391731
David Barrero <davbarfer@gmail.com>
2025-06-08 11:57:25 +0200

    Create base API

--- nodeJS/main.js
+++ nodeJS/main.js
@@ -1,3 +1,56 @@
-// main.js
-console.log('Hello, World!');
+import http from 'node:http'
 
+const processRequest = (req, res) => {
+  const { method, url } = req
+
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/user':
+
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify({
+            name: 'John Doe',
+            age: 30
+          }))
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('Not found')
+      }
+
+    case 'POST':
+      switch (url) {
+        case '/login': {
+          let body = ''
+
+          // escuchar el evento data
+          req.on('data', chunk => {
+            body += chunk.toString()
+          })
+
+          req.on('end', () => {
+            const data = JSON.parse(body)
+            // llamar a una base de datos para guardar la info
+            res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
+
+            data.timestamp = Date.now()
+            res.end(JSON.stringify(data))
+          })
+
+          break
+        }
+
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+          return res.end('Not found')
+      }
+  }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(1234, () => {
+  console.log('server listening on port http://localhost:1234')
+})
