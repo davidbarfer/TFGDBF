1608d7427729a9d467486e18be8ab7efbdd19701
David Barrero <davbarfer@gmail.com>
2025-06-12 19:13:13 +0200

    Create hash password script for testing purposes

---
+++ nodeJS/generatePassHash.mjs
@@ -0,0 +1,8 @@
+import bcrypt from 'bcrypt'
+
+const password = 'DoctusLitePassword'
+
+const hash = bcrypt.hashSync(password, 12)
+console.log(hash)
+
+// $2b$12$W16liLOZR6U4Zp3iptOPEOPNCl8ob/ieZqmEkdOWrrD5yo3qYK5xW
