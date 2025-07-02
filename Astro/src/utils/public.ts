b9e666f88c2b0dc0abcaf27a51a87c13714201bd
David Barrero <davbarfer@gmail.com>
2025-07-02 11:42:44 +0200

    Create a basic error handler

--- Astro/src/utils/public.ts
+++ Astro/src/utils/public.ts
@@ -1,7 +1,8 @@
 export async function getSubject(id: string) {
   const response = await fetch(`http://localhost:1234/subject/${id}`);
   if (!response.ok) {
-    return null
+    const errorText = await response.json();
+    throw new Error(`Error ${response.status}: ${errorText.error}`);
   }
   return response.json();
 }
