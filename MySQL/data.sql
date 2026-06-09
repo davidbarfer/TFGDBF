98dd4426902f549c17a72600dddf7013724ca3ea
David Barrero <davbarfer@gmail.com>
2026-06-09 21:30:00 +0200

    Update degree to all degrees and master of the ETSI

--- MySQL/data.sql
+++ MySQL/data.sql
@@ -1,3 +1,6 @@
+-- 1. Desactivar restricciones temporalmente para poder insertar/limpiar en cualquier orden
+SET FOREIGN_KEY_CHECKS = 0;
+
 -- Insert sample data
 INSERT INTO users (username, password, password_salt, role, name, surname) VALUES
 ('admin', '$2b$12$W16liLOZR6U4Zp3iptOPEOPNCl8ob/ieZqmEkdOWrrD5yo3qYK5xW',12,'admin', 'David', 'Barrero'),
@@ -6,10 +9,31 @@ INSERT INTO users (username, password, password_salt, role, name, surname) VALUE
 ('student2', '$2b$12$W16liLOZR6U4Zp3iptOPEOPNCl8ob/ieZqmEkdOWrrD5yo3qYK5xW',12,'student', 'Manolo', 'Garcia')
 ON DUPLICATE KEY UPDATE username = username;
 
-INSERT INTO degrees (name) VALUES
-('Grado en Ingeniería de Tecnologías Industriales'),
-('Grado en Ingeniería de las Tecnologías de Telecomunicación')
-ON DUPLICATE KEY UPDATE name = name;
+INSERT INTO degrees (id,name) VALUES
+(1, 'Grado en Ingeniería de Tecnologías Industriales (Plan 2024)'),
+(2, 'Grado en Ingeniería de las Tecnologías de Telecomunicación'),
+(3, 'Grado en Ingeniería Aeroespacial'),
+(4, 'Grado en Ingeniería Civil'),
+(5, 'Grado en Ingeniería Química'),
+(6, 'Grado en Ingeniería de Organización Industrial'),
+(7, 'Grado en Ingeniería de la Energía'),
+(8, 'Grado en Ingeniería Electrónica, Robótica y Mecatrónica'),
+(9, '(No se oferta) Grado en Ingeniería de Tecnologías Industriales (Plan 2010)'),
+(10, 'Máster Universitario en Ingeniería Industrial (Plan 2024)'),
+(11, 'Máster Universitario en Ingeniería de Telecomunicación'),
+(12, 'Máster Universitario en Ingeniería Aeronáutica'),
+(13, 'Máster Universitario en Ingeniería de Caminos, Canales y Puertos'),
+(14, 'Máster Universitario en Ingeniería Química'),
+(15, 'Máster Universitario en Diseño Avanzado en Ingeniería Mecánica'),
+(16, 'Máster Universitario en Ingeniería en Edificios, Robótica y Automática'),
+(17, 'Máster Universitario en Organización Industrial y Gestión de Empresas'),
+(18, 'Máster Universitario en Sistemas de Energía Eléctrica'),
+(19, 'Máster Universitario en Sistemas de Energía Térmica'),
+(20, 'Máster Universitario en Ingeniería Ambiental'),
+(21, "Master's Degree in Operation of Space Systems"),
+(22, 'Doble Máster Universitario en Ingeniería Química e Ingeniería Ambiental'),
+(23, "Joint Master's Degree in Aeronautic Engineering and Operation of Space Systems")
+ON DUPLICATE KEY UPDATE name = VALUES(name);
 
 INSERT INTO subject (name, course, degree_id) VALUES
 ('Fundametos de Control Automático', 2, 1),
@@ -50,6 +74,8 @@ INSERT INTO submissions (user_id, practice_id, delivery_date, grade) VALUES
 UPDATE submissions SET feedback = 'Buen trabajo' WHERE id = 1;
 UPDATE submissions SET evaluator_grade = 8 WHERE id = 2;
 
+-- 2. Volver a activar las restricciones de claves foráneas
+SET FOREIGN_KEY_CHECKS = 1;
 
 -- Show tables
 SHOW TABLES;
