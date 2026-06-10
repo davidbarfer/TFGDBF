27c3ce3be95df836482dedb57bc6efcea43900df
David Barrero <davbarfer@gmail.com>
2026-06-10 22:24:31 +0200

    Add subject_ids of professor to API endpoint

--- nodeJS/handler/userGetHandler.mjs
+++ nodeJS/handler/userGetHandler.mjs
@@ -57,6 +57,24 @@ export const getUsersProfessors = async (req, res, params) => {
       res.statusCode = 404;
       return res.end(JSON.stringify({ error: 'No professor found' }));
     }
+    const professors_ids_arrays = professors.results
+      .map(professor => professor.id)
+      .flat();
+    const user_subjects = await query(
+      `SELECT user_id, subject_id from users_subjects WHERE user_id IN (${professors_ids_arrays.map(() => '?').join(',')})`,
+      professors_ids_arrays
+    );
+    if (user_subjects.results.length !== 0) {
+      professors.results.forEach(professor => {
+        professor.subjects_id = user_subjects.results
+          .filter(e => e.user_id === professor.id)
+          .map(e => e.subject_id);
+      });
+    } else {
+      professors.results.forEach(professor => {
+        professor.subjects_id = [];
+      });
+    }
     res.statusCode = 200;
     res.end(JSON.stringify(professors.results));
   } catch (error) {
