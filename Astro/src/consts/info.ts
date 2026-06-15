4e946a4dc30886247d489f7b950b7d34280d8bd2
David Barrero <davbarfer@gmail.com>
2026-06-15 22:56:57 +0200

    Update frameworks to be clickable links

--- Astro/src/consts/info.ts
+++ Astro/src/consts/info.ts
@@ -1,10 +1,12 @@
+import { link } from "node:fs";
+
 const PROJECT_NAME = 'Doctus Lite';
 const PROJECT_AUTHOR = 'David Barrero Fernandez';
 const PROJECT_FRAMEWORKS = {
-  FRONTEND : 'ASTRO',
-  BACKEND  : 'NODE JS',
-  DATABASE : 'MYSQL',
-  FILESYSTEM : 'NODE JS',
+  FRONTEND : {name: 'ASTRO', link: 'https://astro.build/'},
+  BACKEND  : {name: 'NODE JS', link: 'https://nodejs.org/'},
+  DATABASE : {name: 'MYSQL', link: 'https://www.mysql.com/'},
+  FILESYSTEM : {name: 'NODE JS', link: 'https://nodejs.org/'},
 }
 const PROJECT_DESCRIPTION = 'Este proyecto consiste en una aplicación de gestión de grupos de práctica cuyas entregas se evalúan a través de un evaluador diseñado en MATLAB';
 export { PROJECT_NAME, PROJECT_AUTHOR, PROJECT_FRAMEWORKS, PROJECT_DESCRIPTION};
