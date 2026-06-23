70ed53af4c366a621e8009724b62d3837f9cdc99
David Barrero <davbarfer@gmail.com>
2026-06-23 22:58:32 +0200

    Implement the logger in general API and MAIN

--- nodeJS/main.mjs
+++ nodeJS/main.mjs
@@ -1,19 +1,20 @@
 import http from 'node:http';
 import net from 'node:net';
+import { logger } from './logger.mjs';
 import { processRequest } from './api.mjs';
 import { processMatlabRequest, activeConnections } from './matlab.mjs';
 import { launchMatlabClient } from './matlabFunctions.mjs';
 import { generateFileSystem } from './fileSystem.mjs';
 
 const isProduction = process.argv.includes('--prod');
-console.log(
-  `Server starting in [${isProduction ? 'PRODUCTION' : 'DEVELOPMENT'}] mode.`
+logger.info(
+  `Server starting mode: ${isProduction ? 'PRODUCTION' : 'DEVELOPMENT'}`
 );
 
 const server = http.createServer(processRequest);
 
 server.listen(Number(process.env.BACKEND_PORT), process.env.BASE_IP, () => {
-  console.log(
+  logger.info(
     `API server listening on: http://${process.env.BASE_IP}:${process.env.BACKEND_PORT}`
   );
 });
@@ -22,24 +23,23 @@ const matlabServer = net.createServer(processMatlabRequest);
 
 // Start listening on a port
 matlabServer.listen(Number(process.env.MATLAB_PORT), () => {
-  console.log('TCP server listening on: ', matlabServer.address());
+  logger.info(`TCP server listening on: ${matlabServer.address()}`);
   // Check if MATLAB client is connected
   if (activeConnections.size === 0) {
-    console.log('No MATLAB connection found, launching client...');
+    logger.info('No MATLAB connection found, launching client...');
     try {
       launchMatlabClient();
     } catch (matlabError) {
-      console.error('Failed to launch MATLAB:', matlabError);
+      logger.error('Failed to launch MATLAB:', { error: matlabError });
     }
   }
 });
 
-if (isProduction) {
-  generateFileSystem({ isProduction: true }).catch(error => {
-    console.error('Production FS Error:', error);
+generateFileSystem({ isProduction })
+  .then(() => logger.info('File system generated sucessfully'))
+  .catch(error => {
+    logger.error(
+      `${isProduction ? 'Production' : 'Development'} FS Initialization Error`,
+      { error: error.message }
+    );
   });
-} else {
-  generateFileSystem({ isProduction: false }).catch(error => {
-    console.error('Development FS Error:', error);
-  });
-}
