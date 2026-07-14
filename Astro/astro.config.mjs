9eb06fa0bcdb946e4f94fa53c2c6700f2c72672b
David Barrero <davbarfer@gmail.com>
2026-07-14 22:21:49 +0200

    Update astro to 7.0.0 and install node adapter

--- Astro/astro.config.mjs
+++ Astro/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import path from 'node:path';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,9 +12,15 @@ export default defineConfig({
       }
     }
   },
+
   output: 'server',
+
   server: {
     port: 4321,
     host: '0.0.0.0'
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
