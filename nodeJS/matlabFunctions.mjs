b2319c391d2a0828565c8ca7b8e9f32808403f0d
David Barrero <davbarfer@gmail.com>
2026-07-14 22:44:57 +0200

    Delete the idea on using MATLAB http server and handshake API

--- nodeJS/matlabFunctions.mjs
+++ nodeJS/matlabFunctions.mjs
@@ -1,48 +1,6 @@
 import { exec } from 'child_process';
 import path from 'path';
-import { logger } from './logger.mjs';
 export const matlabPath = path.resolve(process.env.MATLAB_PATH);
-export function connectHandshake(request) {
-  const response = {
-    type: 'evaluation_result',
-    result: 1,
-    timestamp: new Date().toISOString(),
-    id: request.id,
-  };
-  return response;
-}
-export const errorHandshake = {
-  type: 'error',
-  message: 'Invalid JSON format',
-  id: null,
-};
-
-export const launchMatlabClient = () => {
-  const matlabClientPath = path.resolve('../MATLAB/TCPclient.m'); // Replace with your MATLAB path
-
-  return new Promise((resolve, reject) => {
-    const matlab = exec(
-      `${matlabPath} -batch "run('${matlabClientPath}')"`,
-      (error, stdout, stderr) => {
-        if (error) {
-          logger.error('MATLAB launch error on launchMatlabClient:', {
-            error: error.message,
-            stack: error.stack,
-          });
-          reject(error);
-          return;
-        }
-        logger.info('MATLAB output on launchMatlabClient:', stdout);
-        if (stderr) {
-          logger.error('MATLAB errors on launchMatlabClient:', stderr);
-        }
-      }
-    );
-
-    // Give MATLAB time to start up and connect
-    setTimeout(() => resolve(matlab), 5000);
-  });
-};
 
 export function executeMatlabFile(filePath) {
   return new Promise((resolve, reject) => {
