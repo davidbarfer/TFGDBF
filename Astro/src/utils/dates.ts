d81230206e921739ff3fe2c029a0776b69e78db1
David Barrero <davbarfer@gmail.com>
2026-07-21 19:40:00 +0200

    Create function to set local date

--- Astro/src/utils/dates.ts
+++ Astro/src/utils/dates.ts
@@ -1,3 +1,6 @@
 export function setISODateYYYYMMDD (date : any) {
   return new Date(date).toISOString().split('T')[0]
 }
+export function setLocalDateLanguage (date : any, language: string = 'es-ES') {
+  return new Date(date).toLocaleDateString(language, { year: 'numeric', month: '2-digit', day: '2-digit' })
+}
